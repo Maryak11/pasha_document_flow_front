5f35f1445e40bbc114ce8649d334f48791a1e2a6
Kirill Guryev <gurev.matvei@gmail.com>
2022-03-28 23:44:13 +0300

    feat: add profile page

---
+++ pages/profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ $auth.user }}</div>
+</template>
+<script>
+export default {
+  layout: 'adminPanel',
+  computed: {
+    auth() {
+      return this.$auth.hasScope('admin');
+    },
+  },
+};
+</script>
+<style scoped></style>
